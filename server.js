3fd847c3f7d68e002db573a0577287facb0ef86d
BasavarajuVB <bellamvadde0393@gmail.com>
2024-12-29 11:51:54 +0530

    updated .env file

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to the database
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('Connected to the database'))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('Connected to the database'))
   .catch((error) => console.error('Error connecting to the database:', error));
 
 // Import routes
