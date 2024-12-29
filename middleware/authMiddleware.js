81dc986ea918d79efacc50e20e9c6885f86f4650
BasavarajuVB <bellamvadde0393@gmail.com>
2024-12-29 12:02:42 +0530

    updated jwt-secret key

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -11,7 +11,7 @@ const protect = async (req, res, next) => {
     if (!token) {
       return res.status(401).json({ message: 'Not authorized, no token' });
     }
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, "BasavaVB");
     const user = await User.findById(decoded.id).select('-password');
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
